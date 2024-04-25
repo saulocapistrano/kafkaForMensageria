132b452873b6623be434f04ca425322c5f86c08c
saulocapistrano <saulocapistrano@gmail.com>
2024-04-25 08:15:08 -0300

    implement callback function by to return partition and offset

--- str-producer/src/main/java/com/strproducer/services/StringProducerService.java
+++ str-producer/src/main/java/com/strproducer/services/StringProducerService.java
@@ -5,15 +5,32 @@ import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 
+@Log4j2
 @Service
 @Component
+@RequiredArgsConstructor
 public class StringProducerService {
 
-	@Autowired
 	private KafkaTemplate<String, String> kafkaTemplate;
 
+	@Autowired
+	public StringProducerService(KafkaTemplate<String, String> kafkaTemplate) {
+		this.kafkaTemplate = kafkaTemplate;
+	}
+
 	public void sendMessage(String message) {
-		kafkaTemplate.send("str-topic", message);
+	    kafkaTemplate.send("str-topic", message).addCallback(
+	        success -> {
+	            if (success != null && success.getRecordMetadata() != null) {
+	                System.out.println("Send message with success: " + message);
+	                System.out.println("Partition: " + success.getRecordMetadata().partition() +
+	                                   ", Offset: " + success.getRecordMetadata().offset());
+	            }
+	        },
+	        error -> System.err.println("Error sending message: " + error.getMessage())
+	    );
 	}
 }
